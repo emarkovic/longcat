ebd7288eb95e97f00bebeaee8a8d7e5362cbf6a1
Ena Markovic <ena.markovic@smartsheet.com>
2019-01-18 15:59:09 -0800

    wip

--- src/index.js
+++ src/index.js
@@ -1,6 +1,10 @@
 var localStorage = window.localStorage;
 var KEY = "longcat-visits"
+
 var catHouse = document.getElementById("cat-tummy");
+var catFacts = document.getElementById("cat-facts")
+
+populateFact();
 var visits = parseInt(localStorage.getItem(KEY));
 if (visits) {
     for (i = 0; i < visits; i++) {
@@ -15,4 +19,12 @@ function addTummy() {
     var img = document.createElement("img");
     img.src = "img/middle.jpg";
     catHouse.appendChild(img);
+}
+
+function populateFact() {
+    fetch("https://catfact.ninja/fact", { mode: "no-cors" })
+        .then(response => response.json())
+        .then(response => {
+            catFacts.innerHTML = response.fact;
+        });
 }
